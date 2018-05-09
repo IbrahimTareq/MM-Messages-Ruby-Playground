b5e9f3d07d1725ebedcb341cdec2d0415831bde7
Ibrahim <ibrahimtareq@live.com>
2018-05-09 16:14:49 +1000

    Update universe.rb

--- ruby-project/universe.rb
+++ ruby-project/universe.rb
@@ -1,9 +1,28 @@
-class Universe
-	def countAllStars(*galaxies)
-		$totalStars = 0
-		galaxies.each { |stars| 
-			$totalStars = stars #fix me!
-		}
-		return $totalStars
-	end
-end
+require 'message_media_messages'
+require 'pp'
+
+# Configuration parameters and credentials
+auth_user_name = 'API_KEY' # The username to use with basic/HMAC authentication
+auth_password = 'API_SECRET' # The password to use with basic/HMAC authentication
+use_hmac = false # Change this to true if you are using HMAC keys
+
+client = MessageMediaMessages::MessageMediaMessagesClient.new(
+    auth_user_name: auth_user_name,
+    auth_password: auth_password,
+    use_hmac: use_hmac
+)
+
+messages = client.messages
+body_value = '{
+   "messages":[
+      {
+         "content":"My first message",
+         "destination_number":"+61491570156"
+      }
+   ]
+}';
+
+body = JSON.parse(body_value);
+
+result = messages.create_send_messages(body)
+pp result
